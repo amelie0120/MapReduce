aa765df0dea81f12a99b32cdd328d763d8e16578
amelie0120 <amalia.adetu@gmail.com>
2024-12-07 11:59:32 +0200

    needs work

---
+++ src/os_threadpool.c
@@ -0,0 +1,714 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "os_threadpool.h"
+//#include "log/log.h"
+//#include "utils.h"
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+// char *strdup(const char *s) {
+//     char *dup = malloc(strlen(s) + 1);
+//     if (dup) {
+//         strcpy(dup, s);
+//     }
+//     return dup;
+// }
+
+FileNode *create_file_node(int nr){
+	FileNode *file = malloc(sizeof(FileNode));
+	if (!file) {
+        perror("Memory allocation failed");
+        exit(1);
+    }
+	file->nr = nr;
+	file->prev = file->next = NULL;
+	return file;
+}
+
+Node *create_node(char *word, int file) {
+	
+    Node *node = malloc(sizeof(Node));
+    if (!node) {
+        perror("Memory allocation failed");
+        exit(1);
+    }
+    node->word = strdup(word);
+    if (!node->word) {
+        perror("Memory allocation failed");
+        free(node);
+        exit(1);
+    }
+	//FileNode *f = create_file_node(file);
+	//node->files = file;
+	insert_last(&node->files, file);
+    node->left = node->right = NULL;
+    return node;
+}
+
+FileNode *insert_last(FileNode **root, int file){
+	FileNode *new = create_file_node(file);
+	if (!(*root)){
+		*root = new;
+		return *root;
+	}
+	
+	//FileNode *filetoinsert = file;
+	FileNode *current = *root;
+	if (file < current->nr){	
+		new->next = (*root);
+		(*root)->prev = new;
+		(*root) = new;
+		return new;
+	}
+	//while (filetoinsert){
+		
+	while (current->next != NULL && current->next->nr < file){
+		current = current->next;
+	}
+
+	if (current->nr == file){
+		return NULL;
+	}
+
+	if (current->next == NULL){
+		new->prev = current;
+		current->next = new;
+
+	}
+	else {
+		//adaug inainte
+		new->next = current->next;
+		new->prev = current;
+		current->next->prev = new;
+		current->next = new;
+	}
+	
+	
+	return (*root);
+}
+
+Node *insert(Node **root, char *word, int file, int reduce) {
+	// FileNode *curr = file;
+	// int nr = 0;
+	// while (curr){
+	// 	curr = curr->next;
+	// 	nr++;
+	// }
+
+    if (!*root){
+		(*root) = create_node(word, file);
+		//(*root)->nr_files = nr;
+		return (*root);
+	}
+
+	Node *current = (*root);
+
+	// if (reduce == 0){
+	// 	Node *nou = create_node(word, file);
+	// 	nou->right = (*root);
+	// 	(*root)->left = nou;
+	// 	(*root) = nou;
+	// 	// while (current->right != NULL){// && strcmp(word, current->word) > 0){
+	// 	// 	current = current->right;
+	// 	// }
+	
+	// 	// nou->left = current;
+	// 	// current->right = nou;
+	
+	// 	return nou;
+	// 	}
+	
+	while (current->right != NULL && strcmp(word, current->word)){
+			current = current->right;
+		}
+	if (strcmp(word, current->word) == 0){
+		//FileNode *fisier = create_file_node(file);
+		//FileNode *curr = current->files;
+		//int nr = 0;
+		//while (curr){
+		insert_last(&current->files, file);
+			//curr = curr->next;
+			//nr++;
+		//}
+		//current->nr_files += nr;
+		return current;
+	}
+
+	Node *nou = create_node(word, file);
+	
+	//nou->nr_files = nr;
+	nou->left = current;
+	current->right = nou;
+	
+	return nou;
+}
+void swap_nodes(Node **head, char *word1, char *word2){
+	// Node *prev1 = NULL, *prev2 = NULL;
+    // Node *curr1 = *head, *curr2 = *head;
+
+    // // Find x and its previous node
+    // while (curr1 && strcmp(curr1->word, word1)) {
+    //     prev1 = curr1;
+    //     curr1 = curr1->right;
+    // }
+
+    // // Find y and its previous node
+    // while (curr2 && strcmp(curr2->word, word2)) {
+    //     prev2 = curr2;
+    //     curr2 = curr2->right;
+    // }
+
+    // // If either x or y is not present, return
+    // if (!curr1 || !curr2) return;
+
+    // // If x is not the head, update prevX->next
+    // if (prev1)
+    //     prev1->right = curr2;
+    // else // Else make y the new head
+    //     *head = curr2;
+
+    // // If y is not the head, update prevY->next
+    // if (prev2)
+    //     prev2->right = curr1;
+    // else // Else make x the new head
+    //     *head = curr1;
+
+    // // Swap next pointers
+    // Node *temp = curr1->right;
+    // curr1->right = curr2->right;
+    // curr2->right = temp;
+}
+
+
+void sort_list(Node **root){
+// 	if (*root == NULL || (*root)->right == NULL)
+// 		return;
+
+// 	//int go = 1;
+// 	Node *curr1 = (*root);
+// 	while (curr1){
+// 		//go = 0;
+// 		Node *curr2 = curr1->right;
+// 		while (curr2){
+// 			// if (curr1 == curr2)
+// 			// 	curr2 = curr2->right;
+// 			// 	continue;
+// 			if (curr1->nr_files == curr2->nr_files){
+// 				if (strcmp(curr1->word, curr2->word) > 0){
+// 					//printf("o sa faca swap intre %s si %s\n", curr1->word, curr2->word);
+// 					// char *word = curr1->word;
+// 					// //int files = curr1->
+// 					// curr1->word = curr2->word;
+// 					// curr2->word = word;
+// 					swap_nodes(root, curr1->word, curr2->word);
+// 					Node *aux = curr1;
+// 					curr1 = curr2;
+// 					curr2 = aux;
+// 					//printf("acum curr1 e %s si curr2 e %s\n", curr1->word, curr2->word);
+// 					//go = 1;
+// 				}
+// 			}
+// 			else if (curr1->nr_files < curr2->nr_files){
+// 				//printf("o sa faca swap intre %s si %s\n", curr1->word, curr2->word);
+// 				swap_nodes(root, curr1->word, curr2->word);
+// 				Node *aux = curr1;
+// 				curr1 = curr2;
+// 				curr2 = aux;
+// 				//printf("acum curr1 e %s si curr2 e %s\n", curr1->word, curr2->word);
+// 				//go = 1;
+// 			}
+// 			curr2 = curr2->right;
+// 			//else if (curr1->nr_files < curr2->nr_files)
+// 		}
+// 		curr1 = curr1->right;
+// 	}
+}
+
+Node *insert_sorted(Node **root, char *word, FileNode *files, int nrfiles){
+	// printf("%s to be inserted\n", word);
+	
+	Node *nou = create_node(word, files->nr);
+	FileNode *aux = files->next;
+	while(aux){
+		printf("insereaza %d pentru %s\n", aux->nr, word);
+		insert_last(&nou->files, aux->nr);
+		aux = aux->next;
+	}
+	FileNode *curr = files;
+	nou->nr_files = 0;
+	while (curr){
+		nou->nr_files++;
+		curr = curr->next;
+	}
+	if (!*root){
+		(*root) = nou;
+		//(*root)->nr_files = 1;
+		// printf("e radacina\n");
+		return (*root);
+	}
+	// printf("nu e primul\n");
+	Node *current = (*root);
+	// while (current != NULL) {
+    //     // If `new_node` should come before `current`
+    //     if (nrfiles > current->nr_files || (nrfiles == current->nr_files && strcmp(word, current->word) < 0)) {
+    //         nou->right = current;
+    //         nou->left = current->left;
+
+    //         if (current->left) {
+    //             current->left->right = nou;
+    //         } else {
+    //             *root = nou; // Update head if inserting at the start
+    //         }
+    //         current->left = nou;
+    //         return nou;
+    //     }
+
+    //     // Move to the next node
+        
+    //     current = current->right;
+    // }
+
+
+	while (current->right != NULL && current->nr_files > nou->nr_files){
+		
+		current = current->right;
+	}
+	while (current->right != NULL && strcmp(word, current->word) > 0 && current->nr_files == nou->nr_files){
+		current = current->right;
+	}
+
+	if (current->right != NULL){
+		if (strcmp(word, current->word) < 0 && nou->nr_files >= current->nr_files){
+			//adaug inainte
+			//Node *new = create_node(word, files);
+			//new->nr_files = 1;
+			if (current == (*root)){
+				nou->right = (*root);
+				(*root)->left = nou;
+				(*root) = nou;
+			}
+			else{
+				nou->right = current;
+				nou->left = current->left;
+				current->left->right = nou;
+				current->left = nou;
+			}
+		}
+		else{
+			//adaug dupa; nu e ult elem sigur
+			nou->left = current;
+			nou->right = current->right;
+			current->right->left = nou;
+			current->right = nou;
+			
+
+		}
+	}
+	else if (current->right == NULL){
+		if (strcmp(word, current->word) < 0 && nou->nr_files >= current->nr_files){
+			//Node *new = create_node(word, files);
+			//new->nr_files = 1;
+			if (current == (*root)){
+				nou->right = (*root);
+				(*root)->left = nou;
+				(*root) = nou;
+			}
+			else{
+				nou->right = current;
+				nou->left = current->left;
+				current->left->right = nou;
+				current->left = nou;
+			}
+			
+		}
+		else {
+			//Node *new = create_node(word, files);
+			//new->nr_files = 1;
+			nou->left = current;
+			current->right = nou;
+		}
+	}
+	return nou;
+
+}
+
+void *reduce(void *arg){
+    ReduceArg *arguments = (ReduceArg *) arg;
+	//printf("litera e %c\n", arguments->letter);
+	
+	char filename[6];
+	sprintf(filename, "%c%s", arguments->letter, ".txt");
+	FILE *file = fopen(filename, "w");
+	//printf("in fisierul %s\n", filename);
+
+	Node *root = NULL;
+	//int nr_nodes = 0;
+	
+	Node *current = arguments->map_big;
+	while (current != NULL){
+		if (current->word[0] == arguments->letter){
+			printf("se adauga %s\n", current->word);
+			insert_sorted(&root, current->word, current->files, current->nr_files);
+			// int nr = 0;
+			// FileNode *curr = current->files;
+			// while (curr){
+			// 	//printf("%d ", curr->nr);
+			// 	curr = curr->next;
+			// 	nr++;
+			// }
+			// current->nr_files = nr;
+		}
+			//nr_nodes++;
+		current = current->right;
+	}
+
+	current = root;
+	while (current){
+		printf("%s:", current->word);
+		//int nr = 0;
+		// FileNode *curr = current->files;
+		// while (curr){
+		// 	printf("%d ", curr->nr);
+		// 	curr = curr->next;
+		// 	//nr++;
+		// }
+		//current->nr_files = nr;
+		printf("are %d fisiere\n", current->nr_files);
+		current = current->right;
+	}
+
+	//sort_list(&root);
+	current = root;
+	while (current){
+		fprintf(file, "%s:[", current->word);
+		int nr = 0;
+		FileNode *curr = current->files;
+		while (curr){
+			nr++;
+			fprintf(file, "%d", curr->nr);
+			if (nr < current->nr_files)
+				fprintf(file, " ");
+			curr = curr->next;
+			
+		}
+		fprintf(file, "]\n");
+		//current->nr_files = nr;
+		//printf("are %d fisiere\n", current->nr_files);
+		current = current->right;
+	}
+	fclose(file);
+    return NULL;
+}
+os_task_t *create_task(void (*action)(void *), void *arg, void (*destroy_arg)(void *))
+{
+	os_task_t *t;
+
+	t = malloc(sizeof(*t));
+	if (t == NULL){
+		perror("malloc");
+		exit(1);
+	}
+
+	t->action = action;		
+	t->argument = arg;		
+	t->destroy_arg = destroy_arg;	
+
+	return t;
+}
+
+void destroy_task(os_task_t *t)
+{
+	if (t->destroy_arg != NULL)
+		t->destroy_arg(t->argument);
+	free(t);
+}
+
+void enqueue_task(os_threadpool_t *tp, os_task_t *t)
+{
+	assert(tp != NULL);
+	assert(t != NULL);
+
+	os_list_node_t *new = &(t->list);
+
+	pthread_mutex_lock(&(tp->mutex));
+	list_add_tail(&tp->head, new);
+	pthread_mutex_unlock(&(tp->mutex));
+
+	if (tp->start == 0) {
+		tp->start = 1;
+		pthread_cond_broadcast(&tp->cond_start_map);
+	} else {
+		pthread_cond_signal(&(tp->condition));
+	}
+}
+
+static int queue_is_empty(os_threadpool_t *tp)
+{
+	return list_empty(&tp->head);
+}
+
+os_task_t *dequeue_task(os_threadpool_t *tp)
+{
+	os_task_t *t;
+
+	pthread_mutex_lock(&tp->mutex);
+	while (queue_is_empty(tp)) {
+		tp->waiting_threads++;
+		
+		if (tp->waiting_threads == tp->num_threads_M) {
+			pthread_cond_broadcast(&tp->condition);
+			pthread_mutex_unlock(&tp->mutex);
+			return NULL;
+		}
+		pthread_cond_wait(&tp->condition, &tp->mutex);
+		if (tp->waiting_threads == tp->num_threads_M) {
+			pthread_mutex_unlock(&tp->mutex);
+			return NULL;
+		}
+		tp->waiting_threads--;
+	}
+
+	t = list_entry(tp->head.next, os_task_t, list);
+
+	list_del(tp->head.next);
+	pthread_mutex_unlock(&tp->mutex);
+
+	return t;
+}
+os_task_t *dequeue_task2(os_threadpool_t *tp)
+{
+	os_task_t *t;
+
+	pthread_mutex_lock(&tp->mutex);
+	while (queue_is_empty(tp)) {
+		tp->waiting_threads++;
+		
+		if (tp->waiting_threads == tp->num_threads_R + tp->num_threads_M) {
+			pthread_cond_broadcast(&tp->condition);
+			pthread_mutex_unlock(&tp->mutex);
+			return NULL;
+		}
+		pthread_cond_wait(&tp->condition, &tp->mutex);
+		if (tp->waiting_threads == tp->num_threads_R + tp->num_threads_M) {
+			pthread_mutex_unlock(&tp->mutex);
+			return NULL;
+		}
+		tp->waiting_threads--;
+	}
+
+	t = list_entry(tp->head.next, os_task_t, list);
+	list_del(tp->head.next);
+	pthread_mutex_unlock(&tp->mutex);
+
+	return t;
+}
+
+void *functie(void *arg){
+	os_threadpool_t *tp = (os_threadpool_t *) arg;
+	while(1){
+		if (tp->finished_threads == tp->num_threads_M){
+			break;
+		}
+		
+	}
+
+	while (1) {
+		os_task_t *t;
+		t = dequeue_task2(tp);
+		if (t == NULL){
+			break;
+		}
+		t->action(t->argument);
+		destroy_task(t);
+	}	
+	
+}
+
+static void *thread_loop_function(void *arg)
+{
+	os_threadpool_t *tp = (os_threadpool_t *) arg;
+	pthread_mutex_lock(&tp->mutex);
+
+	if (tp->start == 0)
+		pthread_cond_wait(&tp->cond_start_map, &tp->mutex);
+
+	pthread_mutex_unlock(&tp->mutex);
+
+	while (1) {
+		os_task_t *t;
+
+		t = dequeue_task(tp);
+		
+		if (t == NULL){
+			pthread_mutex_lock(&tp->mutex_start);
+			
+			if (tp->finished_threads == tp->num_threads_M - 1){
+				// Node *current = tp->map_big;
+				// while (current){
+				// 	printf("%s\n", current->word);
+				// 	current = current->right;
+				// }
+				for (char c = 'a'; c <= 'z'; c++){
+					ReduceArg *arguments = malloc(sizeof(ReduceArg));
+					arguments->letter = c;
+					arguments->map_big = tp->map_big;
+					os_task_t *new_task = create_task((void *)reduce, (void *)arguments, NULL);
+					enqueue_task(tp, new_task);
+				}
+				Node *current = tp->map_big;
+				while (current){
+					printf("%s:", current->word);
+					//int nr = 0;
+					FileNode *curr = current->files;
+					while (curr){
+						printf("%d ", curr->nr);
+						curr = curr->next;
+						//nr++;
+					}
+					printf("\n");
+					//current->nr_files = nr;
+					//printf("are %d fisiere\n", current->nr_files);
+					current = current->right;
+				}
+			}
+			
+			tp->finished_threads ++;
+			pthread_mutex_unlock(&tp->mutex_start);
+			
+			
+			break;
+		}
+		t->action(t->argument);
+
+		ThreadArgs *args = (ThreadArgs *)t->argument;
+		
+		destroy_task(t);
+
+		Node *current = args->map;
+		while(current != NULL){
+			//Node *nou = create_node(current->word, args->id);
+
+			pthread_mutex_lock(&tp->mutex_access_map);
+			//FileNode *nodfisier = create_file_node(args->id);
+			insert(&tp->map_big, current->word, args->id, 0);
+			// Node *current_big = tp->map_big;
+			// tp->number_pairs ++;
+			// if (!current_big){
+			// 	tp->map_big = nou;
+			// }
+			// else{
+			// 	while (current_big->right != NULL){
+			// 		current_big = current_big->right;
+			// 	}
+			// 	nou->left = current_big;
+			// 	current_big->right = nou;
+			// }
+			pthread_mutex_unlock(&tp->mutex_access_map);
+
+			current = current->right;
+		}
+	}
+
+	return NULL;
+}
+
+void wait_for_completion(os_threadpool_t *tp)
+{
+	for (unsigned int i = 1; i <= tp->num_threads_M + tp->num_threads_R; i++)
+		pthread_join(tp->threads[i], NULL);
+		
+}
+
+os_threadpool_t *create_threadpool(unsigned int num_threads_M, unsigned int num_threads_R, int nr_fisiere)
+{
+	os_threadpool_t *tp = NULL;
+	int rc;
+
+	tp = malloc(sizeof(*tp));
+	if (tp == NULL){
+		perror("malloc");
+		exit(1);
+	}
+
+	tp->start = 0;
+	tp->waiting_threads = 0;
+	tp->finished_threads = 0;
+	tp->semafor = 0;
+	tp->number_pairs = 0;
+	//tp->find_letter = 'a';
+	list_init(&tp->head);
+
+	rc = pthread_mutex_init(&tp->mutex, NULL);
+	if (rc != 0){
+		perror("pthread_mutex_init");
+		exit(1);
+	}
+	rc = pthread_mutex_init(&tp->mutex_access_map, NULL);
+	if (rc != 0){
+		perror("pthread_mutex_init");
+		exit(1);
+	}
+	rc = pthread_mutex_init(&tp->mutex_start, NULL);
+	if (rc != 0){
+		perror("pthread_mutex_init");
+		exit(1);
+	}
+
+	pthread_cond_init(&tp->condition, NULL);
+	pthread_cond_init(&tp->cond_start_map, NULL);
+
+	tp->num_threads_M = num_threads_M;
+	tp->num_threads_R = num_threads_R;
+	//tp->list_of_lists = malloc(26 * sizeof(Node *));
+	
+	tp->threads = malloc((tp->num_threads_M + tp->num_threads_R + 1) * sizeof(*tp->threads));
+	if (tp->threads == NULL){
+		perror("malloc");
+		exit(1);
+	}
+	for (int id = 1; id <= num_threads_M + num_threads_R; id++) {
+		if (id <= num_threads_M){
+			rc = pthread_create(&tp->threads[id], NULL, &thread_loop_function, (void *) tp);
+			if (rc < 0){
+				perror("pthread_create");
+				exit(1);
+			}
+		}
+		else{
+			rc = pthread_create(&tp->threads[id], NULL, &functie, (void *) tp);
+			if (rc < 0){
+				perror("pthread_create");
+				exit(1);
+			}
+		}
+        
+		
+    }
+	return tp;
+}
+
+void destroy_threadpool(os_threadpool_t *tp)
+{
+	os_list_node_t *n, *p;
+
+	pthread_mutex_destroy(&tp->mutex);
+	//pthread_mutex_destroy(&tp->mutex_semafor);
+	pthread_mutex_destroy(&tp->mutex_access_map);
+	pthread_mutex_destroy(&tp->mutex_start);
+	pthread_cond_destroy(&tp->cond_start_map);
+	pthread_cond_destroy(&tp->condition);
+	//pthread_cond_destroy(&tp->cond_start_reduce);
+	list_for_each_safe(n, p, &tp->head) {
+		list_del(n);
+		destroy_task(list_entry(n, os_task_t, list));
+	}
+
+	free(tp->threads);
+	free(tp);
+}
---
+++ src/tema1.c
@@ -0,0 +1,134 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "os_threadpool.h"
+
+Node *map(void *arg){
+    ThreadArgs *args = (ThreadArgs *)arg;
+    FILE *file = fopen(args->filename, "r");
+    if (file == NULL){
+        perror("Error opening file");
+        exit(1);
+    }
+    char buffer[256];
+    //printf("File %s opened\n", args->filename);
+    //args->hashmap = malloc(sizeof(args->hashmap));
+
+    // Read the file line by line
+    while (fgets(buffer, sizeof(buffer), file)) {
+        char *token = strtok(buffer, " \n,.:;!?"); // Specify delimiters
+        while (token != NULL) {
+            //printf("%s\n", token);
+            
+            //printf("%s\n", token); // Print each word
+            char token1[strlen(token)];
+            int j = 0;
+            for (int i = 0; token[i] != '\0'; i++) {
+                if (isalpha(token[i]))
+                    token1[j++] = tolower((unsigned char)token[i]);
+                //token[i] =  // Convert each character to lowercase
+            }
+            token1[j] = '\0';
+            //FileNode *file = create_file_node(args->id);
+            insert(&args->map, token1, args->id, 0);
+            token = strtok(NULL, " \n,."); // Continue tokenizing
+        }
+    }
+
+    // Node *current = args->map;
+    // while (current != NULL){
+    //     printf("%s: %d\n", current->word, current->files);
+    //     current = current->right;
+    // }
+    // printf("\n");
+
+    
+    
+    fclose(file);
+    printf("finished %s\n", args->filename);
+    return args->map;
+}
+
+int main(int argc, char **argv){
+    if (argc < 3){
+        perror("Not enough arguments");
+        return 1;
+    }
+
+    int M = atoi(argv[1]);
+    int R = atoi(argv[2]);
+    char buffer[256];
+    
+
+    pthread_mutex_t mutex_index_fisier;
+    pthread_mutex_init(&mutex_index_fisier, NULL);
+    //argv[3] e filename-ul
+    FILE *file = fopen(argv[3], "r");
+    if (file == NULL){
+        perror("Error opening file");
+        exit(1);
+    }
+
+    int nr_fisiere;
+    if (fgets(buffer, sizeof(buffer), file) != NULL){
+        nr_fisiere = atoi(buffer);
+        //printf("%d texte\n", nr_fisiere);
+    }
+
+    char **lines = NULL;
+    lines = malloc((nr_fisiere + 1) * sizeof(char *));
+    if (lines == NULL) {
+        perror("Memory allocation failed");
+        fclose(file);
+        return 1;
+    }
+
+    int lineCount = 1;
+    while (fgets(buffer, sizeof(buffer), file)) {
+        // Remove trailing newline, if present
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        lines[lineCount] = malloc(strlen(buffer) + 1);
+        if (lines[lineCount] == NULL) {
+            perror("Memory allocation failed");
+            break;
+        }
+        strcpy(lines[lineCount], buffer);
+        lineCount++;
+    }
+
+
+    pthread_t threads[M + R];
+    int r;
+    long id;
+    void *status;
+    int ids[M + R];
+
+    ThreadArgs **args = malloc((nr_fisiere + 1) * sizeof(ThreadArgs *));
+    for (int i = 1; i <= nr_fisiere; i++){
+        args[i] = malloc(sizeof(ThreadArgs));
+        if (args[i] == NULL) {
+            perror("Failed to allocate memory for thread arguments");
+            return 1;
+        }
+    }
+
+    os_threadpool_t *tp = create_threadpool(M, R, nr_fisiere);
+
+    for (int i = 1; i <= nr_fisiere; i++) {
+        args[i]->id = i;
+        args[i]->filename = lines[i];
+        args[i]->map = NULL;
+        os_task_t *new_task = create_task((void *)map, args[i], NULL);
+        enqueue_task(tp, new_task);
+        //printf("a fct enqueue pt fisieru %d\n", i);
+    }    
+
+    //printf("aici?\n");
+    wait_for_completion(tp);
+    destroy_threadpool(tp);
+
+    fclose(file);
+    return 0;
+}
